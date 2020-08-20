226d5c2b209c571f893e5df8ee15eea910bb4e44
DDA209 <didier.jeux1@gmail.com>
2020-08-20 18:29:51 +0200

    restart handlebars

--- index.js
+++ index.js
@@ -5,29 +5,34 @@ const app = express();
 
 const port = 3000;
 
-const url = './translation.json'
+// const url = './translation.json'
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars')
 
-fetch(url)
-.then( resp => resp.json() )
-.then( json => {
-    console.log("fetch json", json);
-})
+// fetch(url)
+// .then( resp => resp.json() )
+// .then( json => {
+//     console.log("fetch json", json);
+// })
 
 app.get('/', (req, res) => {
-    res.render('home', {
-        title: 'titre'
-    });
+    res.render('home');
+    // , {
+    //     pageTitle: 'Titre de la page',
+    //     title: 'titre'
+    // }
+    // );
 });
 
+/*
 app.get('/:lang?', (req, res) => {
     res.render('home', {
+        pageTitle: 'Titre de la page',
         title: 'titre'
     });
 })
-
+*/
 app.listen( port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
