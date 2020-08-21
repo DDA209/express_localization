9cb3e9eaa43a4d78991834e9a045ea6a0135c454
DDA209 <didier.pascarel1@gmail.com>
2020-08-21 16:37:27 +0200

    Finishing without bonus

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const translate = require('./translations.json');
 
 const app = express();
 
-const port = 3000;
-
-// const url = './translation.json'
+const port = 3005;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
+app.use(express.static('public'));
+
+// const url = './translations.json'
 // fetch(url)
 // .then( resp => resp.json() )
 // .then( json => {
@@ -17,22 +19,51 @@ app.set('view engine', 'handlebars')
 // })
 
 app.get('/', (req, res) => {
-    res.render('home');
-    // , {
-    //     pageTitle: 'Titre de la page',
-    //     title: 'titre'
-    // }
-    // );
+    console.log('GET /');
+    console.log('GET / req.params', req.params);
+
+
+    res.render('home'
+    , {
+        pageTitle: translate.fr.pageTitle,
+        title: translate.fr.title,
+        image: "fr"
+    }
+    );
 });
 
-/*
+
 app.get('/:lang?', (req, res) => {
-    res.render('home', {
-        pageTitle: 'Titre de la page',
-        title: 'titre'
-    });
+    // console.log('GET /:lang/', lang);
+    console.log('GET /:lang/ req.params', req.params);
+
+    const language = req.params.lang
+    const pageTitle = translate[language].pageTitle;
+    const title = translate[language].title;
+    const image = `img/${language}.png`;
+
+    console.log('GET /:lang/ translate[language]', translate[language]);
+    console.log('GET /:lang/ language', language);
+    // console.log('GET /:lang/ pageTitle', pageTitle);
+    // console.log('GET /:lang/ title', title);
+
+    res.render('home'
+    , {
+        pageTitle,
+        title,
+        image
+    }
+    );
 })
-*/
+
+
+// app.get('*', (req, res) => {
+//     console.log('GET *');
+//     console.log('GET * req.params', req.params);
+
+// });
+
+
 app.listen( port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
